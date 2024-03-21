df5d67154fccc3a49155997f00c97378e6e3d9bb
Neo40 <tron36.cc@gmail.com>
2024-03-20 21:38:08 -0300

    Adição de classe DTO

---
+++ yasp/src/main/java/one/digitalinnovation/santander/yasp/common/dto/BankCardTaxDTO.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.santander.yasp.common.dto;
+
+public class BankCardTaxDTO {
+    private final Long id;
+    private final Double tax;
+
+    public BankCardTaxDTO(final Long id, Double tax) {
+        this.id = id;
+        this.tax = tax;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Double getTax() {
+        return tax;
+    }
+}
