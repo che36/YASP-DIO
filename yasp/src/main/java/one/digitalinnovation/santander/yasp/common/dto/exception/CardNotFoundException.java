e7355e829a8d532daa94589f17467033d53449a1
Neo40 <tron36.cc@gmail.com>
2024-03-29 18:03:14 -0300

    Adiçao de classe de Excessão cartão nao encontrado

---
+++ yasp/src/main/java/one/digitalinnovation/santander/yasp/common/dto/exception/CardNotFoundException.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.santander.yasp.common.dto.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No such card")
+public class CardNotFoundException extends RuntimeException {
+
+}
