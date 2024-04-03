790fdf148aa2644e1a7a84644971816374950482
Neo40 <tron36.cc@gmail.com>
2024-04-03 17:17:05 -0300

    Adição de pacote repository

---
+++ yasp/src/main/java/one/digitalinnovation/santander/yasp/repository/BankCardRepository.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.santander.yasp.repository;
+
+import one.digitalinnovation.santander.yasp.common.dto.entity.BankCard;
+import one.digitalinnovation.santander.yasp.common.dto.entity.TaxableCard;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public interface BankCardRepository {
+    Optional<BankCard> getBankCardById(Long id);
+
+    void create(BankCard bankCard);
+
+    void clear();
+
+    void addEntry(Long id, LocalDateTime date, double value);
+
+    Optional<TaxableCard> getTaxableCardById(Long id);
+
+}
