373bf1946019ecb66dc91996b5d995fbc01042dc
Neo40 <tron36.cc@gmail.com>
2024-04-02 22:20:10 -0300

    Pacote Service da Aplicação adicionado

---
+++ yasp/src/main/java/one/digitalinnovation/santander/yasp/service/BankCardServiceImpl.java
@@ -0,0 +1,22 @@
+package one.digitalinnovation.santander.yasp.service;
+
+import one.digitalinnovation.santander.yasp.common.dto.entity.TaxableCard;
+import one.digitalinnovation.santander.yasp.common.dto.exception.TaxableCardNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class BankCardServiceImpl implements BankCardService {
+    private final BankCardRepository bankCardRepository;
+
+    public BankCardServiceImpl(BankCardRepository bankCardRepository) {
+        this.bankCardRepository = bankCardRepository;
+    }
+
+    @Override
+    public Double getCardTax(Long id) {
+        final Optional<TaxableCard> taxableCardById = bankCardRepository.getTaxableCardById(id);
+        return taxableCardById.map(TaxableCard::getCalculatedTax).orElseThrow(TaxableCardNotFoundException::new);
+    }
+}
